53427a133efe0db731dff35bcb78a59c5f0523f2
figueirass <santiago.figueiras@iteso.mx>
2025-06-03 12:39:47 -0600

    third commit

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ print("------- Normal  Distribuition -------")
 model.bid_ask_normal_distribuition(mu=102, sigma=7, pi=0.3)
 
 print("----- Exponential Distribuition -----")
-model.bid_ask_exponential_distribuition(lam=0.0075, pi=0.1)
+model.bid_ask_exponential_distribuition(lam=0.0075, pi=0.01)
